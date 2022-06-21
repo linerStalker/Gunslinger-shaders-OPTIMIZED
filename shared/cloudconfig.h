5d2a102655a060ec3e0c422ddd87dc35f656b035
linerStalker <103878721+linerStalker@users.noreply.github.com>
2022-06-21 12:58:04 +0300

    Add files via upload

---
+++ shared/cloudconfig.h
@@ -0,0 +1,11 @@
+#ifndef _CLOUDCONFIG_H
+#define _CLOUDCONFIG_H
+
+//note:timers has resolution (sec),where x=1,y=10,z=1/10,
+#define CLOUD_TILE0        (0.7f)
+#define CLOUD_SPEED0       (0.1)
+#define CLOUD_TILE1        (2.8)
+#define CLOUD_SPEED1       (0.05)
+#define CLOUD_FADE         (0.5)
+
+#endif
