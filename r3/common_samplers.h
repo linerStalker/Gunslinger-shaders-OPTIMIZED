7935bd9da4035a99560fc9b0923785bc3f77900c
linerStalker <103878721+linerStalker@users.noreply.github.com>
2022-06-23 12:22:00 +0300

    R3 HOTFIX: Fix issues in commit R3: Fix SMAA
    Fix problems with commit R3: Fix SMAA

--- r3/common_samplers.h
+++ r3/common_samplers.h
@@ -5,7 +5,7 @@ sampler smp_nofilter,smp_rtlinear,smp_linear,smp_base,smp_material;
 
 #ifdef USE_MSAA
 Texture2D	s_base,s_bump,s_detail,s_hemi,s_lmap,s_bloom,s_image,s_tonemap;
-Texture2DMS(float4,MSAA_SAMPLES)	s_generic,s_position,s_normal,s_diffuse,s_accumulator;
+TEXTURE2DMS(float4,MSAA_SAMPLES)	s_generic,s_position,s_normal,s_diffuse,s_accumulator;
 #else
 Texture2D   s_base,s_bump,s_detail,s_hemi,s_lmap,s_bloom,s_image,s_tonemap,s_generic,s_position,s_normal,s_diffuse,s_accumulator;
 #endif
